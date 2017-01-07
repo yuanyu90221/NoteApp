1829823a537114902510285462311ce78db3eeeb
yuanyu90221 <yuanyu90221@gmail.com>
2017-01-07 23:05:27 +0800

    add version id

--- src/com/note/vo/NoteVO.java
+++ src/com/note/vo/NoteVO.java
@@ -13,6 +13,11 @@ import java.util.Locale;
 public class NoteVO implements Serializable{
 	
 	/**
+	 * field SID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
 	 * field :id
 	 */
 	private long _id;
